dc19e92dfa2b8340015a23b44f17cbfb42264aca
dilip <er.dilip.n@gmail.com>
2016-08-21 17:12:53 -0500

    initial spring boot project

---
+++ volunteering_SpringWebMVC_SpringData_JPA_JTA/src/main/java/cs544/mum/edu/VolunteeringSpringWebMvcSpringDataJpaJtaApplication.java
@@ -0,0 +1,12 @@
+package cs544.mum.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteeringSpringWebMvcSpringDataJpaJtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteeringSpringWebMvcSpringDataJpaJtaApplication.class, args);
+	}
+}
